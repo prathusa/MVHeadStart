db319b0d79e35c32c49c1aefee441f1106366bbb
prathusa <35161910+prathusa@users.noreply.github.com>
2020-02-02 19:53:23 -0600

    Updated P Loop Turn Code

--- src/main.cpp
+++ src/main.cpp
@@ -37,19 +37,53 @@ void askPosition(void) {
 //turn method that turns to a target rotation
 void turn(double degrees) //P loop turn code (better than the smartdrive methods once kP is tuned properly)
 {
-  double target = degrees; // In revolutions
-  double error = target - Inertial.rotation();
-  double kP = .6;
-  while (std::abs(error) > 1) //1 allows +- 1 degree variance, don't change unless you know what you are doing
+  if(Inertial.installed())
   {
-    error = target - Inertial.rotation();
-    double percent = kP * error + 10;
-    leftGroup.spin(directionType::fwd, percent, pct);
-	  rightGroup.spin(directionType::rev, percent, pct);
-    vex::task::sleep(20);
+    double target = degrees; // In revolutions
+    double error = target - Inertial.rotation();
+    double kP = .6;
+    if(error > 0)
+    {
+      
+      while (std::abs(error) > 1) //1 allows +- 1 degree variance, don't change unless you know what you are doing
+      {
+        error = target - Inertial.rotation();
+        double percent = kP * error + 15;
+        leftGroup.spin(directionType::fwd, percent, pct);
+	      rightGroup.spin(directionType::rev, percent, pct);
+        vex::task::sleep(20);
+      }
+    }
+    else if(error > 0)
+    {
+      double target = degrees; // In revolutions
+      double error = target - Inertial.rotation();
+      double kP = .6;
+      while (std::abs(error) > 1) //1 allows +- 1 degree variance, don't change unless you know what you are doing
+      {
+        error = target - Inertial.rotation();
+        double percent = kP * error - 15;
+        leftGroup.spin(directionType::fwd, percent, pct);
+	      rightGroup.spin(directionType::rev, percent, pct);
+        vex::task::sleep(20);
+      }
+    }
+    else
+    {
+      leftGroup.stop();
+      rightGroup.stop();
+      goto end;
+    }
   }
-  leftGroup.stop();
-  rightGroup.stop();
+  else
+  {
+    Brain.Screen.clearScreen();
+    Brain.Screen.setFont(fontType::mono40);
+    Brain.Screen.setFillColor(red);
+    Brain.Screen.print("No Inertial Sensor Installed");
+  }
+  end:
+  vex::task::sleep(10);
 }
 
 void pre_auton(void) {
