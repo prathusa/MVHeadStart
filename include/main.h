e788fa91135eec28d4e07b49b75609f42fd29296
prathusa <35161910+prathusa@users.noreply.github.com>
2020-02-23 15:38:52 -0600

    More explainations and restructuring done

---
+++ include/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <unistd.h>
+
+#include "v5.h"
+#include "v5_vcs.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,109 @@
-#include "vex.h"
+#include "main.h"
 int auton = -1;
+
+using namespace vex;
+brain Brain;
+controller Controller1;
+competition Competition;
+
+//Add motor definitions here:
+motor rightFrontMotor (vex::PORT11, vex::gearSetting::ratio18_1, true); //Change the port number to change the port
+motor rightRearMotor (vex::PORT12, vex::gearSetting::ratio18_1, true); //Change the port number to change the port
+motor leftRearMotor (vex::PORT19, vex::gearSetting::ratio18_1, false); //Change the port number to change the port
+motor leftFrontMotor (vex::PORT20, vex::gearSetting::ratio18_1, false); //Change the port number to change the port
+motor placeHolderMotor1 (vex::PORT1, vex::gearSetting::ratio18_1); //Change the port number to change the port
+motor placeHolderMotor2 (vex::PORT2, vex::gearSetting::ratio18_1); //Change the port number to change the port
+motor placeHolderMotor3 (vex::PORT3, vex::gearSetting::ratio18_1); //Change the port number to change the port
+motor placeHolderMotor4 (vex::PORT3, vex::gearSetting::ratio18_1); //Change the port number to change the port
+//motor placeHolderTorqueMotor (vex::PORT3, vex::gearSetting::ratio36_1); //Just change the gearsetting on 
+//                                                            the motor that has a changed insert to match these demo motors
+//motor placeHolderSpeedMotor (vex::PORT3, vex::gearSetting::ratio6_1);
+
+//Add sensor definitions here:
+inertial Inertial(vex::PORT20); //Change the number to change the port
+pot Potentiometer(Brain.ThreeWirePort.A); //Change the letter to change the port
+limit LimitSwitch(Brain.ThreeWirePort.H); //Change the letter to change the port
+
+//Add motor_group/drivetrain definitions here:
+motor_group leftGroup(leftFrontMotor, leftRearMotor);
+motor_group rightGroup(rightFrontMotor, rightRearMotor);
+drivetrain driveTrain(leftGroup, rightGroup);
+smartdrive smartDrive(leftGroup, rightGroup, Inertial, 12.56, 16, 16, distanceUnits::in);
+
+void drive(double distanceInches, int speed) // Speed ranges from 0 to 100
+//                     if you put in a negative number for distance then you will move backwards
+{
+  driveTrain.driveFor(directionType::fwd, distanceInches, distanceUnits::in, speed, velocityUnits::pct);
+}
+
+//turn method that turns to a target rotation
+void turn(double degrees) //P loop turn code (better than the smartdrive methods once kP is tuned properly)
+{
+  if(Inertial.installed())
+  {
+    double target = degrees; // In revolutions
+    double error = target - Inertial.rotation();
+    double kP = .6;
+    if(error > 0)
+    {
+      
+      while (std::abs(error) > 1) //1 allows +- 1 degree variance, don't reduce unless you know what you are doing
+      {
+        error = target - Inertial.rotation();
+        double percent = kP * error + 20;
+        leftGroup.spin(directionType::fwd, percent, pct);
+	      rightGroup.spin(directionType::rev, percent, pct);
+        vex::task::sleep(20);
+      }
+    }
+    else if(error > 0)
+    {
+      double target = degrees; // In revolutions
+      double error = target - Inertial.rotation();
+      double kP = .6;
+      while (std::abs(error) > 1) //1 allows +- 1 degree variance, don't change unless you know what you are doing
+      {
+        error = target - Inertial.rotation();
+        double percent = kP * error - 20;
+        leftGroup.spin(directionType::fwd, percent, pct);
+	      rightGroup.spin(directionType::rev, percent, pct);
+        vex::task::sleep(20);
+      }
+    }
+    else
+    {
+      leftGroup.stop();
+      rightGroup.stop();
+    }
+  }
+  else
+  {
+    Brain.Screen.clearScreen();
+    Brain.Screen.setFont(fontType::mono40);
+    Brain.Screen.setFillColor(red);
+    Brain.Screen.print("No Inertial Sensor Installed");
+  }
+}
+
+void X_Button_Macro()
+{
+
+}
+
+void Y_Button_Macro()
+{
+  
+}
+
+void Left_Button_Macro()
+{
+  
+}
+
+void Right_Button_Macro()
+{
+  
+}
 
 void drive_Tank() {
   leftGroup.spin(vex::directionType::fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
@@ -31,14 +135,22 @@ void controls()
         if(Controller1.ButtonA.pressing())
         {
           //add commands here to complete when button is pressing
+          //Demo code:
+          placeHolderMotor1.spin(forward, 100, percentUnits::pct);
         }
         else if(Controller1.ButtonB.pressing())
         {
           //add commands here to complete when button is pressing
+          //Demo code:
+          placeHolderMotor1.spin(reverse, 100, percentUnits::pct);
         }
         else
         {
           //usually the motor stop command goes here
+          //Demo code:
+          placeHolderMotor1.stop(brakeType::brake); 
+          //You can change the type of brake to better suit the type of system
+          //For example,  use brake type hold for lift/arm mechanisms
         }
 
         if(Controller1.ButtonUp.pressing())
@@ -82,27 +194,28 @@ void controls()
 
         if(Controller1.ButtonX.pressing())
         {
-          //macro button template
+          X_Button_Macro();
         }
 
         if(Controller1.ButtonY.pressing())
         {
-          //macro button template
+          Y_Button_Macro();
         }
 
         if(Controller1.ButtonLeft.pressing())
         {
-          //macro button template
+          Left_Button_Macro();
         }
 
         if(Controller1.ButtonRight.pressing())
         {
-          //macro button template
+          Right_Button_Macro();
         }
 }
 
 // GUI initialization code, don't change unless you know what you are doing
-void askPosition(void) { 
+void askPosition() 
+{ 
   Brain.Screen.clearScreen();
   Brain.Screen.setFont(prop30);
   Brain.Screen.setPenColor(color(180, 180, 180));
@@ -133,58 +246,6 @@ void askPosition(void) {
   Brain.Screen.setPenColor(black);
   Brain.Screen.printAt(335, 195, false, "BBS");
   Brain.Screen.setPenColor(color(180, 180, 180));
-}
-
-//turn method that turns to a target rotation
-void turn(double degrees) //P loop turn code (better than the smartdrive methods once kP is tuned properly)
-{
-  if(Inertial.installed())
-  {
-    double target = degrees; // In revolutions
-    double error = target - Inertial.rotation();
-    double kP = .6;
-    if(error > 0)
-    {
-      
-      while (std::abs(error) > 1) //1 allows +- 1 degree variance, don't change unless you know what you are doing
-      {
-        error = target - Inertial.rotation();
-        double percent = kP * error + 15;
-        leftGroup.spin(directionType::fwd, percent, pct);
-	      rightGroup.spin(directionType::rev, percent, pct);
-        vex::task::sleep(20);
-      }
-    }
-    else if(error > 0)
-    {
-      double target = degrees; // In revolutions
-      double error = target - Inertial.rotation();
-      double kP = .6;
-      while (std::abs(error) > 1) //1 allows +- 1 degree variance, don't change unless you know what you are doing
-      {
-        error = target - Inertial.rotation();
-        double percent = kP * error - 15;
-        leftGroup.spin(directionType::fwd, percent, pct);
-	      rightGroup.spin(directionType::rev, percent, pct);
-        vex::task::sleep(20);
-      }
-    }
-    else
-    {
-      leftGroup.stop();
-      rightGroup.stop();
-      goto end;
-    }
-  }
-  else
-  {
-    Brain.Screen.clearScreen();
-    Brain.Screen.setFont(fontType::mono40);
-    Brain.Screen.setFillColor(red);
-    Brain.Screen.print("No Inertial Sensor Installed");
-  }
-  end:
-  vex::task::sleep(10);
 }
 
 //Auton recorder, do not change at all unless you know what you are doing (except for save file name if needed) 
@@ -220,8 +281,19 @@ void auton_recorder()
     Controller1.Screen.print("Complete!");  
 }
 
-void pre_auton(void) 
+void pre_auton() 
 {
+  
+  if(Inertial.installed())
+  {
+    Inertial.calibrate(2000);
+    vex::task::sleep(2000);
+    Brain.Screen.clearScreen();
+    Brain.Screen.setFont(fontType::mono40);
+    Brain.Screen.setFillColor(red);
+    Brain.Screen.print("Inertial Sensor Calibrating");
+  }
+
   int xLastTouch = Brain.Screen.xPosition();
   int yLastTouch = Brain.Screen.yPosition();
   askPosition();
@@ -295,9 +367,6 @@ void pre_auton(void)
     Controller1.Screen.clearScreen();
     Controller1.Screen.print("No Auton Selected");
   }
-
-  Inertial.calibrate(2000);
-  vex::task::sleep(2000);
 }
 
 void autoRFS()
@@ -325,7 +394,7 @@ void autoSkills()
     //Code Skills auton here:
 }
 
-void autonomous(void)
+void autonomous()
 {
     if(auton == 0)
     {
@@ -397,7 +466,7 @@ void autonomous(void)
     }
 }
 
-void usercontrol(void)
+void usercontrol()
 {
     if(auton == 5) 
     {
